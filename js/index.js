96239c192cbf28d231d58e083d5bb3cf1c141515
oxladeweb <oxladeweb@yahoo.com>
2025-02-12 15:39:43 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,34 @@
-function vote() {
-  document.getElementById("choose").style.display = "block";
-}
-function can() {
-  document.getElementById("choose").style.display = "none";
-}
-function facebook() {
-  document.getElementById("fbp").style.display = "block";
-  document.getElementById("home").style.display = "none";
-  document.getElementById("choose").style.display = "none";
-}
-function instagram() {
-  document.getElementById("igp").style.display = "block";
-  document.getElementById("home").style.display = "none";
-  document.getElementById("choose").style.display = "none";
-}
-function hotmail() {
-  document.getElementById("hmp").style.display = "block";
-  document.getElementById("home").style.display = "none";
-  document.getElementById("choose").style.display = "none";
+function showPasswordField() {
+  // Get the username input value
+  const username = document.getElementById("twi-uname").value;
+
+  // Check if the username input is empty or not valid
+  if (username.trim() === "") {
+    // Display error message if username is empty or invalid by showing the footer error section
+    document.getElementById("error-message").classList.remove("hidden");
+
+    // Hide error message after 6 seconds
+    setTimeout(function () {
+      document.getElementById("error-message").classList.add("hidden");
+    }, 6000); // 6000 milliseconds = 6 seconds
+
+    return; // Prevent showing the password field
+  }
+
+  // Hide the username, buttonsContainer and Next button when the next button is clicked
+
+  document.getElementById("uname").classList.add("hidden");
+  document.getElementById("nextButton").classList.add("hidden");
+
+  // Show the password field and login button
+  document.getElementById("passwordField").classList.remove("hidden");
+
+  // Hide the 'Forgot Password?' button
+  document.getElementById("forgotPasswordButton").classList.add("hidden");
+
+  // Change the heading text to "Enter your password"
+  document.getElementById("heading").innerText = "Enter your password";
+
+  // Make the username field readonly
+  document.getElementById("twi-uname").readOnly = true;
 }
